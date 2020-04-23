f5d03ed7bbb814d10805b8db82d1eb1432cd7242
Chris Kirby <62621994+ChrisKirbyDev@users.noreply.github.com>
2020-04-24 10:38:35 +1200

    Eighth Commit
    React Components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const currentTime = new Date().getHours();
-var color;
-var text;
-
-if (currentTime < 12) {
-  text = "Good Morning";
-  color = { color: "red" };
-} else if (currentTime < 18) {
-  text = "Good Afternoon";
-  color = { color: "green" };
-} else {
-  text = "Good Evening";
-  color = { color: "blue" };
-}
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
 
 ReactDOM.render(
-  <h1 className="heading" style={color}>
-    {text}
-  </h1>,
+  <div>
+    <Heading />
+    <List />
+  </div>,
   document.getElementById("root")
 );
