b1f74c63381fd9d6c7f71f66b1b6f15db556aa4d
Chris Kirby <62621994+ChrisKirbyDev@users.noreply.github.com>
2020-04-24 10:10:16 +1200

    Seventh commit
    React styling practice

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { string } from "prop-types";
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
 
-var today = new Date();
-var hr = today.getHours();
-var min = today.getMinutes();
-var time = hr + ":" + min;
-console.log(time);
+const currentTime = new Date().getHours();
+var color;
+var text;
 
-if (hr < 12) {
-  var greet = "Good morning";
-  var edit = { color: "red" };
-} else if (hr >= 12 && hr < 18) {
-  greet = "Good afternoon";
-  edit = { color: "green" };
-} else if (hr >= 18 && hr < 24) {
-  greet = "Good Evening";
-  edit = { color: "blue" };
+if (currentTime < 12) {
+  text = "Good Morning";
+  color = { color: "red" };
+} else if (currentTime < 18) {
+  text = "Good Afternoon";
+  color = { color: "green" };
+} else {
+  text = "Good Evening";
+  color = { color: "blue" };
 }
 
 ReactDOM.render(
-  <h1 style={edit} className="heading">
-    {greet}
+  <h1 className="heading" style={color}>
+    {text}
   </h1>,
   document.getElementById("root")
 );
